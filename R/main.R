cf7e648c04583c552c2ccde9d05e48ef87972533
vikkki <zhaoxiaofanjiayou@163.com>
2022-06-01 13:09:28 -0700

    modified package info

--- R/main.R
+++ R/main.R
@@ -3,6 +3,7 @@
 #' @title Select cells from clusters
 #' @description  A light weight shiny app tool to enable manually selection of cells from seurat object based on its umap reduction
 #' @param input_seurat_obj a seurat obj with umap reduction
+#' @return a list of chr, selected barcode names
 #' @export
 #' @author Ashley(X) Zhao
 #' @examples xSelectCells(seurat_obj)
@@ -13,11 +14,10 @@ library(Seurat)
 # library(Cairo)   # For nicer ggplot2 output when deployed on Linux
 
 # obj = readRDS("~/Documents/Lizard_tail/GEX/harmony/tl_harmony_fibsub.rds")
-seurat_obj <- c()
 set.seed(4422)
 
 ## functions here ##
-# 
+#
 
 my_color = c("#0B90AA","#7dce94","#B1B336","#04384A","#66638B","#D74B4B","#FF652D","#F6AE2D","#AE8D65","#D8DEAE","#70AB8F")
 
@@ -28,7 +28,7 @@ getPalette = colorRampPalette(my_color)
 #source("app.R")
 xSelectCells <- function(input_seurat_obj) {
   my_barcodes  <- c()
-  
+
   #ui <- server <- NULL # avoid NOTE about undefined globals
   library(shiny)
   ui_1 <- fluidPage(
@@ -44,16 +44,16 @@ xSelectCells <- function(input_seurat_obj) {
     verbatimTextOutput("barcode_brush_info"),
     hr(),
     div(downloadButton("dl_select_cells", "Download selected cell barcodes"), align = "left"),
-    div(actionButton("stop", label = "Comfirm Selection $ Return",
-                     icon("paper-plane"), 
+    div(actionButton("stop", label = "Comfirm Selection & Return",
+                     icon("paper-plane"),
                      style="color: #fff; background-color: #337ab7; border-color: #2e6da4"))
-    
+
   )
-  
+
   server_1 <- function(input, output, session) {
-    
+
     output$umap_for_brush <- plotly::renderPlotly(umap_for_brush())
-    
+
     sc_seurat_umap_inter_base <- reactive({
       sc = input_seurat_obj
       dim(sc)
@@ -65,12 +65,12 @@ xSelectCells <- function(input_seurat_obj) {
       embeds$nFeature_RNA = sc@meta.data[["nFeature_RNA"]]
       embeds$cluster <- sc@meta.data[["seurat_clusters"]]
       embeds$keys <- rownames(embeds)
-      
+
       return(embeds)
     })
-    
+
     umap_for_brush <- reactive({
-      
+
       sc = sc_seurat_umap_inter_base()
       # -- axits settings
       ax <- list(
@@ -78,7 +78,7 @@ xSelectCells <- function(input_seurat_obj) {
         # gridcolor = #bdc3c7,
         # gridwidth = 1
       )
-      
+
       marker = list(size = input$point_size)
       library(dplyr)
       plotly::plot_ly(sc,type="scatter", mode = "markers",
@@ -89,11 +89,11 @@ xSelectCells <- function(input_seurat_obj) {
               marker = marker,
               hoverinfo = "all",
               hovertext = paste0(sc$keys,"\n","nCount:",sc$nCount_RNA,"\n","nFeature:",sc$nFeature_RNA)) %>% plotly::layout(dragmode = "lasso", xaxis = ax, yaxis = ax)
-      
+
     })
-    
+
     cluster_brush_cells <- reactiveVal(NULL)
-    
+
     output$barcode_brush_info <- renderPrint({
       d <- plotly::event_data("plotly_selected")
       if(is.null(d)) "Select from plot tools and drag to select cells (double-click to clear)."
@@ -103,7 +103,7 @@ xSelectCells <- function(input_seurat_obj) {
         paste0(nrow(d)," barcodes have been selected.")
       }
     })
-    
+
     output$dl_select_cells <- downloadHandler(
       filename = function() {
         paste0("Barcode_selected", ".csv")
@@ -123,12 +123,11 @@ xSelectCells <- function(input_seurat_obj) {
 
   #source(file_path, local = TRUE)
   #server_env <- environment(server)
-  #rlang::env_binding_unlock(server_env, nms = NULL)
   #print(server_env)
   #server_env$input_seurat_obj <- input_seurat_obj
-  
+
   #app <- shiny::shinyApp(ui, server)
-  
+
   return(shiny::runGadget(app = ui, server = server))
 
 }
