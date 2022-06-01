c2f2ba71de62bf15556f47ac6ab77b044158a3d6
vikkki <zhaoxiaofanjiayou@163.com>
2022-06-01 10:40:29 -0700

    tring rungadget()

--- R/main.R
+++ R/main.R
@@ -31,7 +31,86 @@ getPalette = colorRampPalette(my_color)
 xSelectCells <-function(input_seurat_obj) {
   #seurat_obj <<- input_seurat_obj
   
-  ui <- server <- NULL # avoid NOTE about undefined globals
+  #ui <- server <- NULL # avoid NOTE about undefined globals
+  ui_1 <- fluidPage(
+    plotlyOutput("umap_for_brush",
+                 width = "auto",
+                 height = "600px",),
+    sliderInput("point_size",
+                "Point size:",
+                min = 0.1,  max = 20, value = 3),
+    hr(),
+    
+    div(downloadButton("dl_select_cells", "Download selected cell barcodes"), align = "left"),
+    hr(),
+    h4("Brushed barcodes"),
+    verbatimTextOutput("barcode_brush_info")
+    
+  )
+  
+  server_1 <- function(input, output, session) {
+    
+    output$umap_for_brush <- renderPlotly(umap_for_brush())
+    
+    sc_seurat_umap_inter_base <- reactive({
+      sc = input_seurat_obj
+      dim(sc)
+      ident = as.data.frame(sc@active.ident)
+      colnames(ident) <- "ident"
+      embeds = as.data.frame(Embeddings(sc[["umap"]]),col.names = T)
+      embeds = cbind.data.frame(embeds, ident)
+      embeds$nCount_RNA  = sc@meta.data[["nCount_RNA"]]
+      embeds$nFeature_RNA = sc@meta.data[["nFeature_RNA"]]
+      embeds$cluster <- sc@meta.data[["seurat_clusters"]]
+      embeds$keys <- rownames(embeds)
+      
+      return(embeds)
+    })
+    
+    umap_for_brush <- reactive({
+      
+      sc = sc_seurat_umap_inter_base()
+      # -- axits settings
+      ax <- list(
+        zeroline = FALSE
+        # gridcolor = #bdc3c7,
+        # gridwidth = 1
+      )
+      
+      marker = list(size = input$point_size)
+      
+      plot_ly(sc,type="scatter", mode = "markers",
+              x = ~UMAP_1, y = ~UMAP_2,
+              key = ~keys,
+              color = ~ident,
+              colors = getPalette(length(levels(sc$ident))),
+              marker = marker,
+              hoverinfo = "all",
+              hovertext = paste0(sc$keys,"\n","nCount:",sc$nCount_RNA,"\n","nFeature:",sc$nFeature_RNA)) %>% layout(dragmode = "lasso", xaxis = ax, yaxis = ax)
+      
+    })
+    
+    cluster_brush_cells <- reactiveVal(NULL)
+    
+    output$barcode_brush_info <- renderPrint({
+      d <- event_data("plotly_selected")
+      if(is.null(d)) "Click and drag to select cells (double-click to clear)."
+      else {
+        cluster_brush_cells(d$key)
+        paste0(nrow(d)," barcodes have been selected.")
+      }
+    })
+    
+    output$dl_select_cells <- downloadHandler(
+      filename = function() {
+        paste0("Barcode_selected", ".csv")
+      },
+      content = function(file) {
+        barcodes = cluster_brush_cells()
+        write.table(barcodes,file, quote = TRUE, sep = ",", col.names = FALSE, row.names = FALSE)
+      }
+    )
+  }
 
   ui <- ui_1
   server <- server_1
@@ -42,8 +121,8 @@ xSelectCells <-function(input_seurat_obj) {
   print(server_env)
   server_env$input_seurat_obj <- input_seurat_obj
   
-  app <- shiny::shinyApp(ui, server)
-  shiny::runApp(app)
+  #app <- shiny::shinyApp(ui, server)
+  shiny::runGadget(app = ui, server = server)
 
 }
 
@@ -63,7 +142,7 @@ ui_1 <- fluidPage(
   
 )
 
-server_1 <- function(input, output) {
+server_1 <- function(input, output, session) {
   
   output$umap_for_brush <- renderPlotly(umap_for_brush())
   
